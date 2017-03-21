0346844c59ddbeb55bd75d114395ef85649155ae
developernabeel <nabeel.bape@fastbanking.com>
2017-03-21 18:48:23 +0530

    Added BasicAlgorithms project
    - Added SortingAlgorithms

---
+++ BasicAlgorithms/BasicAlgorithms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunSortingAlgorithms();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static void RunSortingAlgorithms()
+        {
+            Console.WriteLine("Enter array size");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            Console.WriteLine("Enter " + n + " space separated integers");
+            string[] integers = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToInt32(integers[i]);
+            }
+            new SortingAlgorithms().InsertionSort(array);
+            Console.WriteLine("Sorted array\n" + string.Join(" ", array));
+        }
+    }
+}
---
+++ BasicAlgorithms/BasicAlgorithms/SortingAlgorithms.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicAlgorithms
+{
+    public class SortingAlgorithms
+    {
+        public void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length -1; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public void BubbleSort(int[] array)
+        {
+            for (int i = array.Length -1; i >= 0 ; i--)
+            {
+                for (int j = 0; j <= i-1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public void InsertionSort(int[] array)
+        {
+            if (array.Length == 1)
+                return;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (array[j] < array[j - 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+                    }
+                    else
+                        break;
+                }
+            }
+        }
+    }
+}
